6be35d1b5b9a37ccd4bba4a24b81809d6192f50e
Ivan_M <iamartynov@rambler.ru>
2023-09-18 11:34:04 +0300

    fix: fix of errors

--- statistics/service/src/main/java/ru/practicum/service/handler/ExceptHandler.java
+++ statistics/service/src/main/java/ru/practicum/service/handler/ExceptHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.service.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.service.exception.ErrorResponse;
@@ -12,10 +13,10 @@ import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestControllerAdvice
-public class ExceptionHandler {
+public class ExceptHandler {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(IncorrectSearchParametersException.class)
+    @ExceptionHandler(IncorrectSearchParametersException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse incorrectSearchParametersException(IncorrectSearchParametersException e) {
         log.warn(e.toString());
